f31fe243afa817fc6bff6a6100fcafaf22b82305
dafhome <d.fernalvarez@gmail.com>
2024-05-09 19:56:31 +0200

    commit

--- 1.Zapateria/src/Elegante.java
+++ 1.Zapateria/src/Elegante.java
@@ -1,4 +1,19 @@
 public class Elegante extends Zapato {
     private int largoCordones;
     public Elegante(String marca, String modelo, String material,double talla, double precio, int largoCordones){
-        super( ma
+        super( marca,  modelo,  material, talla,  precio);
+        this.largoCordones=largoCordones;
+    }
+    public int getLargoCordones() {
+        return largoCordones;
+    }
+    public void setLargoCordones(int largoCordones) {
+        this.largoCordones = largoCordones;
+    }
+    @Override
+    public String toString() {
+        return "Zapato [marca=" + marca + ", modelo=" + modelo + ", material=" + material + ", talla=" + talla
+                + ", precio=" + precio +", largo cordones=" + largoCordones + "]";
+    }
+
+}
